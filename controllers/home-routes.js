00ee356d569fac1099c2653501102fd1026ffaa5
Sarah Maskill <sarahnmaskill@gmail.com>
2021-10-28 18:17:38 -0400

    cleaned up code rerouted homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
-
+router.get('/', (req,res) => {
+  res.render('homepage')
+})
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
