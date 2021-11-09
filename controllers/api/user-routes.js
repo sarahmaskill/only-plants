eda90ea4459f04f6fde422860dbbb72bf96748b6
Alexander Leino <Alexander.Leino@yahoo.com>
2021-11-08 20:55:08 -0500

    fixed log in issue

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -41,7 +41,7 @@ router.post('/', async (req, res) => {
   
       req.session.save(() => {
         req.session.loggedIn = true;
-        req.session.dbUserData = id;
+        req.session.user_id = dbUserData.id;
   
         res.status(200).json(dbUserData);
       });
